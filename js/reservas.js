eb59755c5aeb7763af0a9af63d29842cb14bfd7d
EmmaCarro <emmacarrouche@outlook.com>
2023-08-01 22:17:54 -0300

    Agregado al main la pre entrega 3

--- js/reservas.js
+++ js/reservas.js
@@ -1,244 +1,166 @@
-// BIENVENIDA
-alert ("Bienvenido a VeranoFeel Reservas. A continuacion le pediremos datos de contacto y luego podrá elegir sus reservas.")
+// Codigo completo recreado, porque sino era muy confuso. Se reemplazaron todos los alert y prompts con la interaccion con el formulario
 
-// INGRESAR DATOS DE CONTACTO
-
-let operacionDatos = false  // definicion para hacer bucle con el confirm
-let usuariorray = [];   //array vacio para llenarlo con datos del usuario en el bucle
-
-// clase constructora para crear objeto con los datos
-class Usuario {
-    constructor (nombre, email, telefono, adultos, ninios, mascotas) {
-        this.nombre = nombre;
-        this.email = email;
-        this.telefono = telefono;
-        this.adultos = adultos;
-        this.ninios = ninios;
-        this.mascotas = mascotas;
+// Hacemos el formulario visible al clickear el boton "Reservar". El formulario se limpia al aparecer.
+document.getElementById("reservar").addEventListener("click", mostrarFormulario);
+document.getElementById("reservar2").addEventListener("click", mostrarFormulario);
+function mostrarFormulario(event) {
+    event.preventDefault();
+    // const formulario = document.getElementById("formulario");
+    // formulario.reset();
+    document.querySelector(".contenedor-formulario").style.display = "block";
+    document.getElementById("formulario").style.display = "block";
+    document.querySelector(".fondo-oscuro").style.display = "block";
   }
-}
-// alerta reusable para prompts mal ingresados
-function alertaDatoInvalido() {
-    alert("Ingrese un dato válido");
-}
 
-do {  // Inicio del bucle global
-
-    let nombre;
-    let email;
-    let telefono;
-    let adultos;
-    let ninios;
-    let mascotas;
-
-    // Bucles individuales para cada dato
-    while (true) {
-        nombre = prompt("Ingrese su nombre:");
-        if (/^[a-zA-Z]+$/.test(nombre)) {
-        break;
-        }
-        alertaDatoInvalido();
-    };
-
-    while (true) {
-        email = prompt("Ingrese su email. Debe ser un email valido:");
-        if (/^[\w.-]+@[\w.-]+\.\w+$/.test(email)) {
-        break;
-        }
-        alertaDatoInvalido();
-    };
-    
-    while (true) {
-        telefono = parseInt(prompt("Ingrese su numero de contacto:"));
-        if (/^\d+$/.test(telefono)) {
-        break;
-        }
-        alertaDatoInvalido();
-    };
-
-    while (true) {
-        adultos = parseInt(prompt("Ingrese cantidad de adultos:"));
-        if (/^\d+$/.test(adultos)) {
-        break;
-        }
-        alertaDatoInvalido();
-    };
-
-    while (true) {
-        ninios = parseInt(prompt("Ingrese cantidad de menores:"));
-        if (/^\d+$/.test(ninios)) {
-        break;
-        }
-        alertaDatoInvalido();
-    };
-
-    while (true) {
-        mascotas = parseInt(prompt("Ingrese cantidad de mascotas:"));
-        if (/^\d+$/.test(mascotas)) {
-        break;
-        }
-        alertaDatoInvalido();
-    };
-
-// Verificacion de todos los datos ingresados. Negarlo reinicia el bucle. Aceptarlo ejecuta el if siguiente.
-operacionDatos = confirm("¿Son estos datos correctos?" + "\nNombre: " + nombre + "\nEMail: " + email + "\nTelefono: " + telefono + "\nAdultos: " + adultos + "\nNiños: " + ninios + "\nMascotas: " + mascotas );
-
-// if para agregar datos al array con un .push si se confirma el confirm anterior
-if (operacionDatos) {
-    usuariorray.push(new Usuario(nombre, email, telefono, adultos, ninios, mascotas));
-    console.log("Usuario registrado: ", usuariorray[0]);
-};
-
-} while (!operacionDatos || (operacionDatos == null) || (operacionDatos == "ESC"));  // Fin del bucle
-
-
-
-// MENU DE RESERVAS INTERACTIVO
-
-// Se define otraReserva que interactua para hacer el ciclo repetitivo en conjunto con funcionMes()
-let otraReserva = false;
-
-// reserva inicial debe ser siempre 0 pesos porque no tenemos base de datos
-let reserva = 0;
+// Definimos que la reserva es cero, pero la total incluye el precio de la primer noche reservada
+let nuevaReserva = 0;
 let reservaTotal = 0;
+const precio = 8000;
 
-// se definen los numeros de inicio y fin de reserva a elegir en blanco
-// var eneInicial = 0;
-// var eneFinal = 0;
-
-// var febInicial = 0;
-// var febFinal = 0;
-
-// Se reemplazaron definiciones individuales por un array de objetos para cada mes del año
-// Se definen dias de inicio y fin totales como numeros que van a ser usados para saber si hay reservas anteriores
-// Se los define al reves para dar espacio a los dias de cada mes en el calculo de los whiles
-const meses = [
-    { nombre: "ENERO", dias: 31, inicialTotal: 32, finalTotal: 0 },
-    { nombre: "FEBRERO", dias: 28, inicialTotal: 29, finalTotal: 0 },
-    { nombre: "MARZO", dias: 31, inicialTotal: 32, finalTotal: 0 },
-    { nombre: "ABRIL", dias: 30, inicialTotal: 31, finalTotal: 0 },
-    { nombre: "MAYO", dias: 31, inicialTotal: 32, finalTotal: 0 },
-    { nombre: "JUNIO", dias: 30, inicialTotal: 31, finalTotal: 0 },
-    { nombre: "JULIO", dias: 31, inicialTotal: 32, finalTotal: 0 },
-    { nombre: "AGOSTO", dias: 31, inicialTotal: 32, finalTotal: 0 },
-    { nombre: "SEPTIEMBRE", dias: 30, inicialTotal: 31, finalTotal: 0 },
-    { nombre: "NOVIEMBRE", dias: 30, inicialTotal: 31, finalTotal: 0 },
-    { nombre: "DICIEMBRE", dias: 31, inicialTotal: 32, finalTotal: 0 },
-];
-
-// se definen el precio final total, modificable al agregar mas reservas, y uno fijo que se usa en la alerta
-let precio = 8000;
-const fijo = 8000;
-
-// Definiciones para evitar errores
-let mensajeReserva = "";
-let mensajeTotal = "";
-// elegirMes = "";
-
-// alerta de bienvenida donde dice el precio por noche, llamamos al .nombre del usuario registrado en el indice [0] del array usuariorray
-alert("¡Hola " + usuariorray[0].nombre + "!\nLa estadia actual por noche es de $" + fijo + "\nAhora continuemos hacia su reserva...")
-
-
-// Ciclo que repite la reserva
-function funcionMes () {
-    do {
-
-        // Se definen variables a usar, comenzando mesValido como falso
-        let elegirMes;
-        let mesValido = false;
-
-        // Se pide ingresar el mes a reservar dentro del while para que inicie en bucle si el resultado es falso (empieza en falso)
-        while (!mesValido) {
-            // este prompt debe ir en toUpperCase porque el .nombre del array es todo uppercase
-            elegirMes = prompt("Escriba el mes en el que desea hacer la reserva a continuacion:").toUpperCase();
-            
-            // se usa el metodo .some para verificar que el nombre sea valido
-            mesValido = meses.some((mes) => mes.nombre === elegirMes);
-
-            // se agrega un if para dar una alerta si el resultado es falso. Se movio la funcion mensajes hasta aca
-            if (!mesValido) {
-            errorMes = elegirMes + " no es un mes valido. Por favor ingrese un nombre de mes valido."
-            mensajes(errorMes,errorMes);
-            }
-        }
-        
-        // usando find para encontrar el .nombre del mes que sea igual al prompt anterior
-        const mesSeleccionado = meses.find ( (mes) => mes.nombre === elegirMes );
-
-        // se ejecuta un bucle con la funcion de elegir los dias del mes
-        if (mesSeleccionado) {
-            elegirDias(mesSeleccionado);
-        } else {                 
-            otraReserva = confirm("Error en los datos ingresados. ¿Reiniciar?");
-        }
-
-        otraReserva = confirm("¿Desea realizar otra reserva?");
-    } while (otraReserva);
+// Cargamos el array de reservas del localStorage. Y si no existe, se crea un array vacio nuevo.
+let reservasArray = JSON.parse(localStorage.getItem("reservas"));
+if (!Array.isArray(reservasArray)) {
+  reservasArray = [];
 }
 
-// Eleccion de los dias llamado por el ciclo, segun el mes elegido
-function elegirDias(mes) {
-    
-    // Definicion de mensajes reutilizables para inicio y fin de la reserva. Se eliminaron un par redundantes para acortar codigo gracias al array
-    const mensajeInicio = `Reservando en mes de ${mes.nombre} \n\nIngrese el dia de inicio de su reserva:`;
-    const mensajeFin = `Reservando en mes de ${mes.nombre} \n\nIngrese el dia final de su reserva:`;
-    
-    // Se reemplazo el switch por simples whiles, acortando el codigo a la mitad gracias al uso del array de objetos
-    // se piden dia inicial y final chequeando que sean validos segun la cantidad de dias de ese mes
-    let inicial = Number.parseInt(prompt(mensajeInicio));
-    while ( 
-            inicial === "" || 
-            inicial === null || 
-            Number.isNaN(Number(inicial)) || 
-            inicial < 1 ||
-            inicial > mes.dias ||
-            (inicial >= mes.inicialTotal && inicial <= mes.finalTotal)
-        ) {
-            alert(`El día de reserva inicial de ${mes.nombre} ingresado no es válido.`);
-            inicial = Number.parseInt(prompt(mensajeInicio));
-    }
+// Llamamos al boton "enviar" del formulario y hacemos que se ejecute todo cuando lo clickeamos
+document.getElementById("enviar").addEventListener("click", function (event) {
+    event.preventDefault();  //evitamos que se ejecute la funcion original del boton que indica el html
 
-    let final = Number.parseInt(prompt(mensajeFin));
-    while (
-            final === "" ||
-            final === null ||
-            Number.isNaN(Number(final)) ||
-            final < 1 ||
-            final > mes.dias ||
-            final < inicial ||
-            (final >= mes.inicialTotal && final <= mes.finalTotal)
-        ) {
-            alert(`El día de reserva final de ${mes.nombre} ingresado no es válido.`);
-            final = Number.parseInt(prompt(mensajeFin));
-    }
+  // Definimos variables
+  let nombres = document.getElementById("nombres").value.trim();
+  let apellidos = document.getElementById("apellidos").value.trim();
+  let email = document.getElementById("email").value.trim();
+  let telefono = parseInt(document.getElementById("telefono").value.trim());
+  let adultos = parseInt(document.getElementById("adultos").value.trim());
+  let ninios = parseInt(document.getElementById("ninios").value.trim());
+  let fechaDesde = parseInt(document.getElementById("fecha-desde").value.trim());
+  let fechaHasta = parseInt(document.getElementById("fecha-hasta").value.trim());
+  let mesSeleccionado = document.getElementById("select-mes").value;
+  let casaSeleccionada = document.getElementById("select-casa").value;
 
-    // calculo del precio total. La reservaTotal suma todas las reservas hasta el momento.
-    let reserva = (final - inicial) * precio;
-    reservaTotal += reserva;
+  // Chequeamos que todos los campos del formulario esten completos y sean validos.
+  // Originalmente habia un solo if con un mensaje generico, pero lo separe aunque fuera mas codigo para poder identificar donde hay un problema
+  if (!nombres || !apellidos || !email || !telefono || !adultos || !ninios || !fechaDesde || !fechaHasta) {
+    mostrarError("Algunos de los campos están vacíos.");
+    console.log("Algunos de los campos están vacíos.")
+    return;}
+  if (!esNumeroEntero(adultos) || !esNumeroEntero(ninios) || !esNumeroEntero(fechaDesde) || !esNumeroEntero(fechaHasta)) {
+    mostrarError("Los numeros ingresados no pueden tener decimales.");
+    console.log("Los numeros ingresados no pueden tener decimales.")
+    return;}
+  if (adultos <= 0) {
+    mostrarError("Debe haber un mínimo de un adulto en cada reserva.");
+    console.log("Debe haber un mínimo de un adulto en cada reserva.")
+    return;}
+  if (!/^[a-zA-Z]+$/.test(nombres) || !/^[a-zA-Z]+$/.test(apellidos) || !/^[\w.-]+@[\w.-]+\.\w+$/.test(email) || !/^\d+$/.test(telefono)) {
+    mostrarError("Algunos datos ingresados no son válidos.\nNombres y Apellidos deben contener letras.\nEmail debe ser válido. Telefono debe contener numeros.");
+    console.log("Nombres y Apellidos deben contener letras. Email debe ser válido. Telefono debe contener numeros.")
+    return;}
+  if (fechaDesde < 1 || fechaDesde > fechaHasta) {
+    mostrarError("La fecha inicial de reserva no puede ser mayor a la final ni menor a 1.");
+    console.log("La fecha inicial de reserva no puede ser mayor a la final ni menor a 1.")
+    return;}
+  if (fechaDesde > obtenerDiasDelMes(mesSeleccionado) || fechaHasta > obtenerDiasDelMes(mesSeleccionado)) {
+    mostrarError("Las fechas no pueden estar fuera del rango de dias del mes elegido.");
+    console.log("Las fechas no pueden estar fuera del rango de dias del mes elegido.")
+    return;}
 
-    const mensajeReserva = `Reservaste tu estadía de ${final - inicial} días desde el ${inicial} hasta el ${final} de ${mes.nombre} por un total de $${reserva}.`;
-    const mensajeTotal = `\nTu estadia completa es por un total de $${reservaTotal}.`;
-    mensajes(mensajeReserva,mensajeReserva);
-    mensajes(mensajeTotal,mensajeTotal);
+  // Verificar si ya existe una reserva para el mes seleccionado
+  const reservaExistente = reservasArray.find((reserva) => 
+    reserva.mesSeleccionado === mesSeleccionado && 
+    reserva.casaSeleccionada === casaSeleccionada
+  );
 
-    // Se define que el total de dias es igual a los dias que se han reservado antes durante bucles de esta funcion
-    mes.inicialTotal += inicial;
-    mes.finalTotal += final;
+  // If que se ejecuta si reservaExistente es true por encontrar que hay reservas en el mes seleccionado con el .find anterior
+  // Se comprueba si las fechas reservadas coinciden con las del formulario que escribio el usuario, es decir si intersectan
+  if (reservaExistente) {
+    const intersectanDias = reservasArray.some((reserva) =>
+        reserva.mesSeleccionado === mesSeleccionado && 
+        reserva.casaSeleccionada === casaSeleccionada &&
+        ((fechaDesde <= reserva.fechaHasta && fechaHasta >= reserva.fechaDesde) || 
+         (fechaHasta <= reserva.fechaHasta && fechaHasta >= reserva.fechaDesde))
+      );
+      // Si los dias intersectan, se ejecuta el error
+      if (intersectanDias) {
+        mostrarError("Algunos de los días elegidos ya están reservados.");
+        console.log("Algunos de los días elegidos ya están reservados.")
+        return;
+      }
+  }
+  
+  // Calcular el precio de la reserva
+  // Calculo del precio total. La reservaTotal suma todas las reservas hasta el momento. Y le agrego +precio una vez por reserva para que reserve el dia que le falta al calculo
+  let nuevaReserva = precio + (fechaHasta - fechaDesde) * precio;
+  reservaTotal += nuevaReserva;
+
+  // Mostrar cartel de confirmación de reserva
+  mostrarConfirmacion(nombres, fechaDesde, fechaHasta, reservaTotal);
+
+  // Agregar la nueva reserva al array de reservas y guardar en localStorage
+  reservasArray.push({mesSeleccionado, fechaDesde, fechaHasta});
+  localStorage.setItem("reservas", JSON.stringify(reservasArray));
+  });
+
+// Funcion para obtener el numero maximo de dias de cada mes, para el chequeo correcto de "fechaDesde" y "fechaHasta". Lo hice con un else if porque ocupaba menos caracteres que un switch
+// Destructuracion del if para acortar aun mas el codigo
+function obtenerDiasDelMes(mesSeleccionado) { 
+    return mesSeleccionado === "febrero" ? 28 : mesSeleccionado === "noviembre" ? 30 : 31;
+  }
+  
+// Defino funciones utiles que voy a reusar
+// Para chequear que sean numeros enteros
+function esNumeroEntero(valor) {
+    return /^\d+$/.test(valor);
+  }
+  
+// Funcion para cerrar carteles de error
+function cerrarCartel() {
+  const carteles = document.querySelectorAll(".alert");
+  carteles.forEach(carte => carte.remove());
 }
 
-// Para los mensajes de consola y alertas, tipo echo
-function mensajes(alerta,consola) {
-    alert(alerta);
-    console.log(consola);
+// Para crear una ventana de error con un "mensaje" customizado
+//el style="white-space: pre-line;" fuerza a que nos de bola el uso de "\n" saltos de linea en el texto del mensaje... porque no queria andar
+function mostrarError(mensaje) {
+    const errorDiv = document.createElement("div");
+    errorDiv.className = "alert alert-danger";
+    errorDiv.style.zIndex = "1001";
+    errorDiv.style.position = "fixed";
+    errorDiv.style.top = "50%";
+    errorDiv.style.left = "50%";
+    errorDiv.style.transform = "translate(-50%, -50%)";
+    errorDiv.innerHTML = `
+    <p style="white-space: pre-line;">${mensaje}</p><br>
+    <button style="display: block; margin: 0 auto;" onclick="cerrarCartel()">Reintentar</button>
+    `;
+    document.body.appendChild(errorDiv);
+  }
+
+// Funcion para cerrar cartel de confirmacion y formulario
+function cerrarTodo() {
+  const carteles = document.querySelectorAll(".alert");
+  carteles.forEach(carte => carte.remove());
+  document.querySelector(".contenedor-formulario").style.display = "none";
+  document.getElementById("formulario").style.display = "none";
+  document.querySelector(".fondo-oscuro").style.display = "none";
 }
-
-
-// Llamado al ciclo de la reserva para iniciar todo
-funcionMes ();
-
-// Despedida
-if (otraReserva === false) {
-    alert("¡Muchas gracias por elegirnos!\n  - VeranoFeel");
-} else {
-    console.log("Reiniciando.")
+// Funcion para crear cartel de confirmacion al crear una reserva correctamente
+function mostrarConfirmacion(nombres, fechaDesde, fechaHasta, reservaTotal) {
+  let mesArreglo = document.getElementById("select-mes").value.toUpperCase();
+  const confirmacionDiv = document.createElement("div");
+  confirmacionDiv.className = "alert alert-success";
+  confirmacionDiv.style.zIndex = "1000";
+  confirmacionDiv.style.position = "fixed";
+  confirmacionDiv.style.top = "50%";
+  confirmacionDiv.style.left = "50%";
+  confirmacionDiv.style.transform = "translate(-50%, -50%)";
+  confirmacionDiv.innerHTML = `
+  <p>¡Reserva realizada para ${nombres}!</p>
+  <p>Desde el ${fechaDesde} hasta el ${fechaHasta} de ${mesArreglo}</p>
+  <p>Precio total de todas las reservas: $${reservaTotal}</p>
+  <button style="display: block; margin: 0 auto;" onclick="cerrarTodo()">Aceptar</button>
+  `;
+  document.body.appendChild(confirmacionDiv);
+  console.log(`Se realizo una reserva desde el ${fechaDesde} hasta el ${fechaHasta} de ${mesArreglo}, y el total hasta ahora es $${reservaTotal}`)
 }
